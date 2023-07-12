57270463401e52f1e63cc7421543435343ae2aba
andy840119 <andy840119@gmail.com>
2023-07-12 16:36:31 +0800

    feat(ui-component): make the pure-JS code into angular component.

--- awsome-switch-button/src/ui-component/switch-button/switch-button.component.ts
+++ awsome-switch-button/src/ui-component/switch-button/switch-button.component.ts
@@ -1,11 +1,49 @@
-const BUTTON = document.querySelector("button");
-const SYNC = document.querySelector("#sync")
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {Component} from "@angular/core";
 
-const TOGGLE = () => {
-  const IS_PRESSED = BUTTON.matches("[aria-pressed=true]");
-  if (SYNC.checked)
-    document.body.setAttribute("data-dark-mode", IS_PRESSED ? false : true);
-  BUTTON.setAttribute("aria-pressed", IS_PRESSED ? false : true);
-};
+type TOnChange = (checked: boolean) => void;
 
-BUTTON.addEventListener("click", TOGGLE);
+@Component({
+  selector: 'switch-button',
+  templateUrl: './switch-button.component.html',
+  styleUrls: ['./switch-button.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: SwitchButtonComponent,
+      multi: true,
+    },
+  ],
+})
+export class SwitchButtonComponent implements ControlValueAccessor {
+  protected checked = false;
+  private onChange: TOnChange | null = null;
+
+  disabled?: boolean;
+
+  constructor() {
+  }
+
+  handleCheckboxInputChange(e: Event): void {
+    const button = e.target as HTMLInputElement;
+    this.checked = button.matches("[aria-pressed=true]");
+
+    if(this.onChange)
+      this.onChange(this.checked);
+  }
+
+  writeValue(checked: boolean): void {
+    this.checked = checked;
+  }
+
+  registerOnChange(fn: TOnChange): void {
+    this.onChange = fn;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  registerOnTouched(): void {}
+
+  setDisabledState(isDisabled: boolean) {
+    this.disabled = isDisabled;
+  }
+}
